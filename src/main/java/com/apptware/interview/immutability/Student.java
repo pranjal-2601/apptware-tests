5620bd3c8b6645274920f6ace6a571a5d786a047
Pranjal <pranjal@Pranjals-MacBook-Air.local>
2024-08-31 00:36:02 +0530

    fix: made changes for fix failing test cases

--- src/main/java/com/apptware/interview/immutability/Student.java
+++ src/main/java/com/apptware/interview/immutability/Student.java
@@ -1,15 +1,32 @@
 /** This class is expected to be immutable. Please make necessary changes. */
 package com.apptware.interview.immutability;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class Student {
-  private String name;
-  private Date dateOfBirth;
-  private List<String> courses;
+
+public final class Student {
+  @Getter
+  private final String name;
+  private final Date dateOfBirth;
+
+  private final List<String> courses;
+
+    public Student(String name, Date dateOfBirth, List<String> courses) {
+        this.name = name;
+        this.dateOfBirth = new Date(dateOfBirth.getTime());
+        this.courses = new ArrayList<>(courses);
+    }
+
+    public Date getDateOfBirth() {
+    return new Date(dateOfBirth.getTime());
+  }
+
+  public List<String> getCourses() {
+    return new ArrayList<>(courses);
+  }
 }
